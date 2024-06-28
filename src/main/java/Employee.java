5617d48ade2f527dec1bfda0ddfee634729f9379
Dashanimus <dashanimus@gmail.com>
2024-06-28 21:56:40 +0300

    Lesson 2 UPD Commit

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,13 +1,12 @@
 public class Employee {
 
-    private final String fullName;
-    private final String position;
-    private final String email;
-    private final String phone;
-    private final double salary;
-    private final int age;
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private double salary;
+    private int age;
 
-    // Конструктор принимающий все поля класса
     public Employee(String fullName, String position, String email, String phone, double salary, int age) {
         this.fullName = fullName;
         this.position = position;
@@ -16,8 +15,8 @@ public class Employee {
         this.salary = salary;
         this.age = age;
     }
+
     public void info() {
         System.out.println("Name: "+fullName+" | Position: "+position+" | email: "+email+" | Phone: "+phone+" | Salary: "+salary+" | Age: "+age);
     }
-
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Задание 2, массив из 5 сотрудников
+        // Задание 2
         Employee employee1 = new Employee("Dashanimus", "Corporate Clown", "dashka@clown.ru", "8-800-555-35-35", 0, 23);
         employee1.info();
+
         Employee[] employees = new Employee[5];
         employees[0] = new Employee("\nIvanov Ivan", "QA-Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
         employees[1] = new Employee("Aston Astonov", "Boss", "aston@example.by", "777777777", 99999, 15);
@@ -12,7 +13,6 @@ public class Main {
         employees[3] = new Employee("Michael Jackson", "Designer", "kingof@style.com", "+19582009", 55100, 50);
         employees[4] = new Employee("Olga Buzova", "Manager", "wtfsinger@spb.ru", "+19862001", 10000, 39);
 
-        // Вывод информации о сотрудниках
         for (Employee employee : employees) {
             employee.info();
             System.out.println();
