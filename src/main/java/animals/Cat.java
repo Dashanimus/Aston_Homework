0b0079f1328eccd82f92f79837ec2520cd3e0e31
Dashanimus <dashanimus@gmail.com>
2024-06-28 22:12:44 +0300

    Lesson 3 UPD Commit

--- src/main/java/animals/Cat.java
+++ src/main/java/animals/Cat.java
@@ -1,30 +1,27 @@
 package animals;
 
 public class Cat extends Animal {
-    public final static int runRangeMax = 200; // Ограничение на бег для кота
-    static int cnt = 0; // Счетчик для подсчета котов
-    public boolean satiety; // Сытость кота
-    public int gluttony; // Количество еды, необходимое коту, чтобы стать сытым
+    public final static int runRangeMax = 200;
+    static int cnt = 0;
+    public boolean satiety;
+    public int gluttony;
 
-    // Класс миски с едой вложенный, т. к. кормим только котов
     public static class Feeder {
-        private int feed; // Количество еды в миске
+        private int feed;
 
-        // Метод для пополнения миски на n еды
         public void fillFeeder(int n) {
             feed = feed + n;
         }
 
-        // Конструктор для создания уже наполненной едой миски
         public Feeder(int feed) {
             this.feed = feed;
         }
     }
 
     public Cat() {
-        cnt++; // Увел. значение на счётчике созданных котов
-        this.satiety = false; // Коты создаются голодными
-        this.gluttony = (int) ((Math.random() * 9) + 1); // Рандомно устанавливаем прожорливость кота от 1 до 10
+        cnt++;
+        this.satiety = false;
+        this.gluttony = (int) ((Math.random() * 9) + 1);
     }
 
     @Override
@@ -37,24 +34,20 @@ public class Cat extends Animal {
     public void run(int range) {
         super.run(range);
         if (range <= runRangeMax) {
-            // Если дистанция <= максимальной, то всё успешно
             System.out.println("Кот пробежал " + range + " метров.");
         } else {
-            // Иначе - превышение ограничения
             System.out.println("Дистанция слишком большая. " + range + " > " + runRangeMax);
         }
     }
 
     public void feedCat(Feeder feeder){
-        // Проверка на сытость кота
         if (this.satiety) {
             System.out.println("Кот сыт, количество еды в миске не изменилось.");
         }
 
-        // Если для кота в миске достаточно еды
         if (feeder.feed >= this.gluttony) {
-            feeder.feed -= this.gluttony; // уменьшаем количество еды в миске
-            this.satiety = true; // кот теперь сыт
+            feeder.feed -= this.gluttony;
+            this.satiety = true;
             System.out.print("Кот теперь сыт. ");
         } else {
             System.out.print("Коту не хватило еды. ");
